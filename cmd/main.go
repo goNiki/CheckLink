c0c713bc94a6cf325fad9c879d0e67ff517e270a
goNiki <gogolevnikita123@gmail.com>
2025-11-10 23:56:34 +0300

    feat(main): Иннициализация логера, handler, подготовка к запуск сервер

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	linkshandler "goNiki/CheckLink/internal/http/handler/links"
+	"goNiki/CheckLink/internal/infrastructure/logger"
+	"goNiki/CheckLink/internal/infrastructure/logger/sl"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
+	log := logger.NewLogger()
+	log.Info("logger initialized")
 
-	fmt.Println("Hello World")
+	handler := linkshandler.NewLinksHandler(log)
+
+	r := chi.NewRouter()
+	r.Post("/links", handler.CheckLink)
+
+	srv := http.Server{
+		Addr:    ":8081",
+		Handler: r,
+	}
+	log.Info("Поднимаю сервер на 8081")
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Error("Ошибка запуска сервера: ", sl.Error(err))
+	}
+
+	log.Info("Сервер остановлен")
+
 }
